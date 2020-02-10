8e883a209b2caf4da4be423a1478df2c5c39f351
robelMike <robelmichael102@gmail.com>
2020-02-10 11:39:50 +0100

    Cant find resource.player

--- app_.py
+++ app_.py
@@ -4,8 +4,8 @@ from flask_jwt import JWT, jwt_required
 import sqlite3
 
 from security_ import authenticate, identity
-from resource.player import Player, Playerlist
 from resource.users_ import UserRegister
+from resource.player import Player, Playerlist
 from resource.club import Club, Clublist
 from flask_restful import Resource, reqparse
 
