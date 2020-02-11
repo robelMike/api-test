e0d1ed016533c3dbf60fbdee8bb3a22bf4461b75
robelMike <robelmichael102@gmail.com>
2020-02-11 09:47:23 +0100

    fixed app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request 
 from flask_restful import Resource, Api
 from flask_jwt import JWT, jwt_required
@@ -11,7 +13,7 @@ from flask_restful import Resource, reqparse
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL, sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
