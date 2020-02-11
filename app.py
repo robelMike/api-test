3ac4fd770a5806b62e446ef881f85bab4044bbf2
robelMike <robelmichael102@gmail.com>
2020-02-11 11:58:22 +0100

    small change

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from flask_restful import Resource, reqparse
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL, sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
